992d07eace6077d5ec1e12a54f75d825bd74644f
dell <dell@DESKTOP-9D011CU>
2021-11-12 12:28:52 +0530

    My 1st commit

---
+++ Selenium_Project/src/test/java/Jenkins/PracticeJenkins.java
@@ -0,0 +1,20 @@
+package Jenkins;
+
+import org.testng.annotations.Test;
+
+public class PracticeJenkins {
+	@Test
+	public void pracJenkins()
+	{
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+	}
+
+}
